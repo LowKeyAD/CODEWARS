86305d58546a52f89440017b9c7264f499d3a3bd
Anthony <101023695+LowKeyAD@users.noreply.github.com>
2022-05-11 18:29:49 -0400

    Add files via upload

---
+++ Get the mean of an array 8kyu.js
@@ -0,0 +1,13 @@
+//Problem: It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+//Return the average of the given array rounded down to its nearest integer.
+//The array will never be empty.
+
+
+//Solution:
+
+function getAverage(arr){
+    //reduce our arr
+    let total = arr.reduce((acc,c)=>acc+c)
+    //divide our arr by length and also Math.floor
+    return Math.floor(total/arr.length)
+}
