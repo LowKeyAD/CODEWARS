b3a1359e4ec991597b0125354eebfba641f470d1
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2022-05-13 16:17:33 -0400

    Add files via upload

---
+++ Even or Odd 8kyu.js
@@ -0,0 +1,17 @@
+
+
+//Problem: Create a function that takes an integer as an argument and returns 
+//"Even" for even numbers or "Odd" for odd numbers.
+
+
+
+//Solution: 
+
+function even_or_odd(number) {
+    if (number % 2) {
+      return "Odd";
+    } else {
+      return "Even";
+    }
+  }
+
