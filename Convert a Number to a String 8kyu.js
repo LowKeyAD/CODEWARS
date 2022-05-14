36c03381bd3fa1a2938a08dc6546cb446403f066
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2022-05-14 16:50:54 -0400

    Add files via upload

---
+++ Convert a Number to a String 8kyu.js
@@ -0,0 +1,10 @@
+
+
+//Problem: We need a function that can transform a number into a string. What ways of achieving this do you know?
+
+
+//Solutions: 
+
+function numberToString(num) {
+    return num.toString();
+}
