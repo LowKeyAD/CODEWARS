0d62886775f1943584f317429ce08ab64ba00458
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2022-07-08 19:22:58 -0400

    Add files via upload

---
+++ Reversed Words 8kyu.js
@@ -0,0 +1,10 @@
+
+
+//Problem: Complete the solution so that it reverses all of the words within the string passed in.
+
+
+//Solution: 
+
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ')
+}
