e1151dd2437ffecfef25945a4c1bd436b3b59ea5
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2022-07-20 19:13:22 -0400

    Add files via upload

---
+++ Grasshopper Personalized Message 8kyu.js
@@ -0,0 +1,16 @@
+
+
+//Problem: Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+//Use conditionals to return the proper message:
+
+//case	return
+//name equals owner	'Hello boss'
+//otherwise	'Hello guest'
+
+
+
+
+//Solutions:
+
+greet = (name, owner) => 'Hello ' + (name == owner ? 'boss' : 'guest')
