30d78637b8eff1b842395e75645ca29242fed7b6
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2022-08-07 17:10:02 -0400

    Add files via upload

---
+++ Jennys Secret Message 8kyu.js
@@ -0,0 +1,14 @@
+
+
+
+//Problem: Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+//Can you help her?
+
+
+
+
+
+//Solution: 
+
+greet = name => name == 'Johnny' ? 'Hello, my love!' : `Hello, ${name}!`
