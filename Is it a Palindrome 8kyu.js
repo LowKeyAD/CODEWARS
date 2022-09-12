c353d932ea6339855d34d4c5fcecef35a95dedfd
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2022-09-12 19:23:47 -0400

    Add files via upload

---
+++ Is it a Palindrome 8kyu.js
@@ -0,0 +1,10 @@
+
+
+//Problem: Write a function that checks if a given string (case insensitive) is a palindrome.
+
+
+//Solution: 
+
+function isPalindrome(x) {
+    return x.toLowerCase() == x.toLowerCase().split('').reverse().join('')
+}
