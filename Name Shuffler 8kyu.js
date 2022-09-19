68c85e89cd8066ac1798d05d85a4f3c885565266
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2022-09-19 18:22:38 -0400

    Add files via upload

---
+++ Name Shuffler 8kyu.js
@@ -0,0 +1,14 @@
+
+
+//Problem: Write a function that returns a string in which firstname is swapped with last name.
+
+//Example(Input --> Output)
+
+//"john McClane" --> "McClane john"
+
+
+
+
+//Solution: 
+
+nameShuffler = str => str.split(' ').reverse().join(' ')
