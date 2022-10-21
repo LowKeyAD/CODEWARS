2a88a8df518aba547e78112166e45ba46869faa0
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2022-10-21 15:28:02 -0400

    Add files via upload

---
+++ Grasshopper Function syntax debugging 8kyu.js
@@ -0,0 +1,14 @@
+
+
+//Problem:
+
+
+//A student was working on a function and made some syntax mistakes 
+//while coding. Help them find their mistakes and fix them.
+
+//Solution:
+
+function main(verb, noun){
+    let addBoth = verb + noun
+    return addBoth
+}
