d5b7be5bb565de9ea0d888f6d3d0f9f3b3773b92
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2022-10-27 14:14:45 -0400

    Add files via upload

---
+++ Simple validation of a username with regex 8kyu.js
@@ -0,0 +1,20 @@
+
+
+//Problem:
+
+/*
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+
+*/
+
+//Solution: 
+
+
+function validateUsr(username) {
+    return /^[0-9a-z_]{4,16}$/.test(username)
+}
