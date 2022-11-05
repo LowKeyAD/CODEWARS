6390740df5ae717dabfb1cd87ac760634446d520
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2022-11-05 13:56:54 -0400

    Add files via upload

---
+++ Grasshopper Variable Assignment Debug 8kyu.js
@@ -0,0 +1,24 @@
+
+
+//Problem:
+
+/*
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+*/
+
+
+//Solution:
+
+var a = "dev"
+var b = "Lab"
+
+var name = a.concat("",b)
+
+
+//or
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
