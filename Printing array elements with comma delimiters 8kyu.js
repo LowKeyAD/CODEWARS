05712bb351db8a6a7b6af159a4bc6e0102e9269c
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2023-01-06 14:37:47 -0500

    Add files via upload

---
+++ Printing array elements with comma delimiters 8kyu.js
@@ -0,0 +1,25 @@
+
+
+//Problem:
+
+/*
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+*/
+
+//Solution:
+
+function printArray(array){
+    return array.toString()
+}
+
+//or
+
+function printArray(array){
+    return array.join()
+}
