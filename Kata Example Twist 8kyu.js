fbdfb929c8eebd2acadcf36bfefb31dcc3122800
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2023-01-08 12:21:02 -0500

    Add files via upload

---
+++ Kata Example Twist 8kyu.js
@@ -0,0 +1,17 @@
+
+
+//Problem:
+
+/*
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+*/
+
+//Solution:
+
+var websites = []
+for(i=0; i < 1000; i++ ){
+  websites.push("codewars")
+}
+
