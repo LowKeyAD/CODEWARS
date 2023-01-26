50298f4b08344389ec8e47d9dc6d1578e69f0fa9
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2023-01-25 21:26:10 -0500

    Add files via upload

---
+++ Exclamation marks series 6 Remove n exclamation marks in the sentence from left to right 8kyu.js
@@ -0,0 +1,22 @@
+
+
+//Problem:
+
+/*
+
+Remove n exclamation marks in the 
+sentence from left to right. n is positive integer.
+
+
+*/
+
+
+//Solution:
+
+function remove(s,n){
+    for(let i = 0; i < n; i++){
+      s = s.replace("!","")
+   }
+     return s
+}
+   
