8ce8da6ec1a700cb24d281f495f8838e51094ccc
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2023-04-05 23:04:59 -0400

    Add files via upload

---
+++ Form the Minimum 7kyu.js
@@ -0,0 +1,19 @@
+//Problem:
+
+/*
+
+Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+
+Notes:
+Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+
+
+*/
+
+
+
+//Solution:
+
+function minValue(values) {
+  return parseInt([...new Set(values)].sort().join(''))
+}
