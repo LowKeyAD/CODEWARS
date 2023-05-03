e66c5c00a4db8b4479b2bdc714b9859bceb32142
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2023-05-03 11:32:52 -0400

    Add files via upload

---
+++ Most Digits 7kyu.js
@@ -0,0 +1,18 @@
+//Problem:
+
+/*
+
+Find the number with the most digits.
+
+If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+
+*/
+
+//Solution:
+
+
+function findLongest(array){
+  let orderArr = array.sort((a,b) =>  b.toString().length - a.toString().length)
+  return orderArr[0]
+}
