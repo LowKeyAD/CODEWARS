bdef3b0f3821acd66059387b3bf1a7b3eb6081a7
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2023-05-07 13:50:54 -0400

    Add files via upload

---
+++ Maximum Product 7kyu.js
@@ -0,0 +1,24 @@
+//Problem:
+
+/*
+
+Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+Notes
+Array/list size is at least 2.
+
+Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+
+*/
+
+
+//Solution:
+
+function adjacentElementsProduct(array) {
+  let newArr = [];
+  for(let i = 0; i < array.length - 1; i++){
+    newArr.push(array[i] * array[i + 1])
+  }
+  return Math.max(...newArr)
+}
