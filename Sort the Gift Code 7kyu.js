9b5acb81946c91f708985dec14b4e93886e8410e
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2023-05-10 12:04:45 -0400

    Add files via upload

---
+++ Sort the Gift Code 7kyu.js
@@ -0,0 +1,21 @@
+//Problem:
+
+/*
+
+Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+Examples (Input -- => Output):
+"abcdef"                      -- => "abcdef"
+"pqksuvy"                     -- => "kpqsuvy"
+"zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+
+*/
+
+
+
+//Solution:
+
+function sortGiftCode(code){
+  let orderMe = code.split("").sort().join("")
+  return orderMe
+}
