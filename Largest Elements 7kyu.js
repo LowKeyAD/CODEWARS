8f918f2335fafa0ce9a93d1f2d1504476c225369
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2023-06-01 12:48:19 -0400

    Add files via upload

---
+++ Largest Elements 7kyu.js
@@ -0,0 +1,19 @@
+//Problem:
+
+/*
+
+Write a program that outputs the top n elements from a list.
+
+Example:
+
+largest(2, [7,6,5,4,3,2,1])
+// => [6,7]
+
+*/
+
+//Solution:
+
+function largest(n, array) {
+    
+  return array.sort((a,b)=>b-a).slice(0, n).reverse();
+}
