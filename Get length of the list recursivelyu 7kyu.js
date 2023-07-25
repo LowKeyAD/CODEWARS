4d424badbc8ba7316aaf7e7dd493b8e70803ad2a
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2023-07-25 14:32:21 -0400

    Add files via upload

---
+++ Get length of the list recursivelyu 7kyu.js
@@ -0,0 +1,18 @@
+//Problem:
+
+/*
+
+Write funcion lenR which returns the length of a given list. Try no to cheat and 
+provide recursive solution.
+
+
+
+*/
+
+//Solution:
+
+
+function lenR(x) {
+  let nuList = x.length;
+  return nuList
+}
