a9f4ccf7dcc9e506e40b8b561da66a51034da841
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2023-09-06 11:37:27 -0400

    Add files via upload

---
+++ Break camelCase 6kyu.js
@@ -0,0 +1,18 @@
+//Problem:
+
+/*
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+*/
+
+//Solution:
+
+function solution(string) {
+    return string.replace(/[A-Z]/g, val => ' ' + val)
+}
