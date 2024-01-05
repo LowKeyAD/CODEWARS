8e197665c6ed50a1c4fe146ea75d833c5178d2ec
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2024-01-04 21:24:07 -0500

    Add files via upload

---
+++ Factorial 7kyu.js
@@ -0,0 +1,19 @@
+//Problem:
+
+/*
+
+Your task is to write function factorial.
+
+
+*/
+
+//Solution:
+
+function factorial(n){
+ let sum = 1;
+  for (let i = 1; i <= n; i++){
+    sum = sum * i;
+  }
+  return sum; 
+
+}
